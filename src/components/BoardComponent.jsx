8803b6e3956d50bdcd444584b4c98c7528411519
FyteBit <Fytebitsoftware@gmail.com>
2024-12-14 11:00:42 +0500

    init

---
+++ src/components/BoardComponent.jsx
@@ -0,0 +1,11 @@
+
+
+const BoardComponent = () => {
+    return (
+        <div className='flex flex-wrap w-[calc(64px*8)] h-[calc(64px*8)] border-3 border-emerald-300 bg-white'>
+            
+        </div>
+    );
+};
+
+export default BoardComponent;
---
+++ src/components/CellComponent.jsx
@@ -0,0 +1,13 @@
+
+
+const CellComponent = () => {
+    const whiteStyles = "flex items-center justify-center w-[64px] h-[64px] bg-slate-50"
+    const blackStyles = "flex items-center justify-center w-[64px] h-[64px] bg-green-400"
+    return (
+        <div className={whiteStyles}>
+            
+        </div>
+    );
+};
+
+export default CellComponent;
